30ab71b720f632ed51ad2edc8a4534960223f9a1
Walder Andreas <andreas.walder@web.de>
2020-02-08 18:43:34 +0100

    Update MsSQLArchiv.php

--- libs/MsSQLArchiv.php
+++ libs/MsSQLArchiv.php
@@ -134,7 +134,7 @@ trait Database
 		$date = date('Y-m-d H:i:s');
 		//$query = 'CREATE TABLE [' . $VarId . '] (id BIGINT PRIMARY KEY, ' . $Typ . 'timestamp DATETIME)';
 		//$query = 'INSERT INTO [' . $table . '] (ParentId,ChildId,KeyValue,Description,Value,Unit,LastUpdate) VALUES ([' . $VarId . '], [' . $VarId . '], ' . $VarIdWert . ', ' . $VarIdWert . ',[' . $VarIdWert . '],' . $VarIdWert . ',' . $date . ')';
-		$query = 'INSERT INTO [' . $table . '] (ParentId) VALUES (123)';
+		$query = 'INSERT INTO [' . $table . '] (ParentId) VALUES (' . $table . ')';
 		try {
 			 $stmt = $conn->query( $query );
 			}
