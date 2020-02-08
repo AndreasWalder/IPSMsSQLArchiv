6da45e1a150f6f284509762a0c4d8ef5e8bdb5db
Walder Andreas <andreas.walder@web.de>
2020-02-08 09:43:16 +0100

    test

--- libs/MsSQLArchiv.php
+++ libs/MsSQLArchiv.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 
 
-require_once __DIR__  . '/../libs/helper/BufferHelper.php') . '}');
-require_once __DIR__  . '/../libs/helper/DebugHelper.php') . '}');
+require_once __DIR__  . '/../libs/helper/BufferHelper.php';
+require_once __DIR__  . '/../libs/helper/DebugHelper.php';
 
 /*
  * @addtogroup mysqlarchiv
