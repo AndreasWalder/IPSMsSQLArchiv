eb763247f238d4fcf10a432bc8562f4b914cdeff
Walder Andreas <andreas.walder@web.de>
2020-02-08 13:58:13 +0100

    Update MsSQLArchiv.php

--- libs/MsSQLArchiv.php
+++ libs/MsSQLArchiv.php
@@ -38,18 +38,16 @@ trait Database
 	  if ($this->ReadPropertyString('Database') == '') {
        return false;
       }
-		$serverName = $this->ReadPropertyString('Host');
-		$database = $this->ReadPropertyString('Database');
-		$conn = new PDO( "sqlsrv:server=$serverName;Database=$database", NULL, NULL);   
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		$query = 'DBCC CHECKDB [' . $database . ']';
 		try {
-		   $stmt = $conn->query($query);
-			}
-		catch( PDOException $err ) {
-		   return false;
-		}  
-		 return true;
+			$serverName = $this->ReadPropertyString('Host');
+            $database = $this->ReadPropertyString('Database');
+			$conn = new PDO( "sqlsrv:server=$serverName;Database = $database", NULL, NULL);   
+			$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		}
+		catch( PDOException $e ) {
+			die( "Error connecting to SQL Server" );
+		}	 
+		return true;
 	}
 		
 	
