41876f54939fd374e5d32a8b1b382617cb558194
Walder Andreas <andreas.walder@web.de>
2020-02-08 19:58:40 +0100

    Update MsSQLArchiv.php

--- libs/MsSQLArchiv.php
+++ libs/MsSQLArchiv.php
@@ -131,7 +131,7 @@ trait Database
 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 		$table = $this->ReadPropertyString('Table');	
 		$VarIdWert = '1';
-		$date = date('d/m/Y H.i.s');
+		$date = date('d/m/Y H.i');
 		echo $date;
 		//$query = 'CREATE TABLE [' . $VarId . '] (id BIGINT PRIMARY KEY, ' . $Typ . 'timestamp DATETIME)';
 		//$query = 'INSERT INTO [' . $table . '] (ParentId,ChildId,KeyValue,Description,Value,Unit,LastUpdate) VALUES ([' . $VarId . '], [' . $VarId . '], ' . $VarIdWert . ', ' . $VarIdWert . ',[' . $VarIdWert . '],' . $VarIdWert . ',' . $date . ')';
