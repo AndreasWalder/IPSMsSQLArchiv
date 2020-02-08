fdd266c92f6965260981c54c07818f13f7004065
Walder Andreas <andreas.walder@web.de>
2020-02-08 21:58:47 +0100

    Update MsSQLArchiv.php

--- libs/MsSQLArchiv.php
+++ libs/MsSQLArchiv.php
@@ -138,7 +138,7 @@ trait Database
 		$table = $this->ReadPropertyString('Table');	
 		$ParentId = $this->ReadPropertyInteger('ParentId');
 		$VarName = IPS_GetName($VarId);
-		$Test = '1';
+		$Test = 'Test';
 		echo $VarName;
 		$VarIdWert = '1';
 		$query = 'INSERT INTO ['.$table.'] (ParentId,ChildId,KeyValue,Description,Value,Unit,LastUpdate) VALUES('.$ParentId.','.$VarId.',CONVERT(nvarchar(MAX),'.$Test.'),'.$VarIdWert.','.$VarIdWert.','.$VarIdWert.',GETDATE())';
