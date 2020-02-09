9c0ebec23ec796635f0505d42bbf36bbf71418fa
Walder Andreas <andreas.walder@web.de>
2020-02-09 12:14:52 +0100

    Update MsSQLArchiv.php

--- libs/MsSQLArchiv.php
+++ libs/MsSQLArchiv.php
@@ -127,14 +127,13 @@ trait Database
 			 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 			 $stmt = $conn->query($query);
 			 $result = $stmt->fetch(PDO::FETCH_ASSOC);
-			 //if ($result <> '') {return;}
+			 if ($result <> '') {return;}
 			 print_r($result);
 			}
 		catch( PDOException $err ) {
 			echo $err;
 		    return false;
 		}  	
-		return true;
 		
         switch ($VarTyp) {
             case VARIABLETYPE_INTEGER:
